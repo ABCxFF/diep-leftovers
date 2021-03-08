c5773c72c6b57a056231448285a853d2ae89c7c1
ABC <79597906+ABCxFF@users.noreply.github.com>
2021-03-07 19:58:31 -0500

    init

--- automators/tank_consts.js
+++ automators/tank_consts.js
@@ -1,1 +1,8 @@
+!((script=document.head.appendChild(document.createElement('script'))) => {
+  script.src='https://cdn.jsdelivr.net/gh/qwokka/wail@latest/wail.min.js';
 
+  return script;
+})().onload= () => {
+  //   
+
+};
